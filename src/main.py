1f5d18595912f388dc16bb048be21fc5908b5959
MohamedKhalilAssa <assaddiki.mohamed1@gmail.com>
2024-12-17 19:42:07 +0100

    prettyfying buy func

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import discord
 from dotenv import load_dotenv
+import json
 import os
 from GamingFunctionality.fetchingData import finalizing_recommendations, scrape_deals
 
@@ -19,9 +20,9 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+    # /buy command
     if message.content.startswith('/buy'):
         message_content = message.content.split(' ')[1:]
-        print(len(message_content))
         if(len(message_content) != 2 or message_content[0].isalnum() == False or  message_content[1].isnumeric() == False):
             await message.channel.send('Invalid Input (ex: /buy PS5 1000)')
             return
@@ -29,21 +30,26 @@ async def on_message(message):
         if code == -1:
             await message.channel.send('Only Tech/Gaming related recommendations are allowed')
             return
-        for index, recomm in enumerate(recomms):
-            links = "\n".join(recomm["links"])  
-            await message.channel.send(
-                f"""
-                ---------------------------------------
-                **Recommendation Number** : {index + 1}
-                **Product's Name** : {recomm["name"]}
-                **Product's Price** : {recomm["actual_price"]}
-                **Product's Description** : {recomm["desc"]}
-                **Product's Links** :
-                {links}
-                """
-            )
+        try :
+          with open("src/GamingFunctionality/logs.txt", "a") as f:
+                log_entry = f'@{message.author} asked for {message_content[0]} with a budget of ${message_content[1]}\n'
+                f.write(log_entry)
+        except Exception as e:
+            print("Error during logging: ", e)
         
-        await message.channel.send('Done')
+        colors = [discord.Color.blue(), discord.Color.green(), discord.Color.red(), discord.Color.purple(),discord.Color.teal()]
+
+        for index, recomm in enumerate(recomms):
+            embed = discord.Embed(
+                title=f"Recommendation #{index + 1}",
+                description=recomm["desc"],
+                color=colors[index % len(colors)]  
+            )
+            embed.add_field(name="Product Name", value=recomm["name"], inline=False)
+            embed.add_field(name="Price", value=recomm["actual_price"], inline=False)
+            embed.add_field(name="Links", value="\n".join([f"- {link}" for link in recomm["links"]]), inline=False)
+            await message.channel.send(embed=embed)
+        await message.channel.send('Recommendations have been sent successfully!')
 
 
 if __name__ == "__main__":
