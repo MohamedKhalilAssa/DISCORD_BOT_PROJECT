cc3a6172fa63fc7bbcf0965dd194064bc2ee29b1
mehdiK05 <mehdi05medo@gmail.com>
2024-12-17 23:21:49 +0100

    completing mods funcs

--- src/BasicFunctionality/mods.py
+++ src/BasicFunctionality/mods.py
@@ -56,3 +56,34 @@ async def unban(ctx, *, username):
             await ctx.guild.unban(user) # unban the user
             await ctx.send(f"{user} has been unbanned.")
     await ctx.send("User not found in the ban list.")
+
+@bot.command()
+@commands.has_permission(moderate_members = True)
+
+async def mute(ctx, member: discord.Member, duration, *, reason = None):
+    time = discord.utils.utcnow() + discord.timedelta(duration)
+    await member.timeout(utili = time, reason = reason) # Apply timeout
+    await ctx.send(f"{member.mention}has been muted for{duration} minutes.")
+
+#command : delete a specified numer of messages from a channel
+
+@bot.command()
+@commands.has_permission(manage_messages = True)
+
+async def purge(ctx, amount):
+    await ctx.channel.urge(limit = amount + 1) #delelte the messages includes the command itself
+    await ctx.send(f"Deleted {amount} messages", delete_after = 3)
+
+#handling permission errors
+
+@bot.event
+async def command_error(ctx, error):
+    if isinstance(error, commands.MissingPermission): #if the error(exception object) is in the exception class commands.MissingPermission
+        await ctx.send("You don't have permission to use this command")
+
+#Anti spam :
+
+#Spam settings :
+message_limits = 5 #maximum messages allowed in the time window
+time_window  = 10 #10 secondes
+mute_duration = 600 # mute for 10 minutes
